db95bb336dfcf84d989934f69cefbe058df49f00
Holman Fonseca <125622151+holfon@users.noreply.github.com>
2023-05-12 21:34:53 -0500

    Inicio Fase 4

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function resultado() {
+    var p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, nota;
+
+    // 1a pregunta
+    if (document.getElementById('p30').checked==true) {p1=1}
+    else {p1=0}
+    // 2a pregunta
+    if (document.getElementById('p32').checked==true) {p2=1}
+    else {p2=0}
+    // 3a pregunta
+    if (document.getElementById('p35').checked==true) {p3=1}
+    else {p3=0}
+    // 4a pregunta
+    if (document.getElementById('p10').checked==true) {p4=1}
+    else {p4=0}
+    // 5a pregunta
+    if (document.getElementById('p13').checked==true) {p5=1}
+    else {p5=0}
+    // 6a pregunta
+    if (document.getElementById('p16').checked==true) {p6=1}
+    else {p6=0}
+    // 7a pregunta
+    if (document.getElementById('p19').checked==true) {p7=1}
+    else {p7=0}
+    // 8a pregunta
+    if (document.getElementById('p21').checked==true) {p8=1}
+    else {p8=0}
+    // 9a pregunta
+    if (document.getElementById('p26').checked==true) {p9=1}
+    else {p9=0}
+    // 10a pregunta
+    if (document.getElementById('p28').checked==true) {p10=1}
+    else {p10=0}
+
+    for(let p = 1; p <= total; p++){
+        if (myForm["r" + p].value == null ||
+        myForm["r" + p].value == ''){
+            alert('Favor responder todas las preguntas');
+            return false;
+        } else {
+            if (myForm[nota=p1+p2+p3+p4+p5+p6+p7+p8+p9+p10].value === respuestasCorrectas [p + 1])
+                nota++;
+        }
+    }           
+
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML= 'Obtuviste: ' +nota + 'puntos';
+    return false;
+}
